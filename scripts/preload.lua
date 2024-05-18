8d4f0f55c834aad6989a0f4bf41f5b5f2b3d2c74
Arthur Cosentino <arthurcose@gmail.com>
2024-05-18 16:42:51 -0400

    Fix bn assets location

--- scripts/preload.lua
+++ scripts/preload.lua
@@ -1,3 +1,3 @@
 Net:on("player_connect", function(event)
-  Net.provide_package_for_player(event.player_id, "/server/mods/dependencies/libraries/battle_network_assets")
+  Net.provide_package_for_player(event.player_id, "/server/mods/mods/libraries/battle_network_assets")
 end)
