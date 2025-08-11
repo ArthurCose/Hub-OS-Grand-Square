b3efc23c54bc54996f039fa55892d3b0a053e38f
Arthur Cosentino <arthurcose@gmail.com>
2025-08-11 19:59:05 -0400

    Add unused balcony

---
+++ assets/tiles/balcony.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="balcony" tilewidth="64" tileheight="48" tilecount="4" columns="2">
+ <tileoffset x="0" y="16"/>
+ <image source="balcony.png" width="128" height="96"/>
+</tileset>
