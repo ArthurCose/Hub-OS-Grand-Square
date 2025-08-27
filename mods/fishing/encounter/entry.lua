d179efd02470848c1c0bcd5d7448ebb26fc0fa40
Arthur Cosentino <arthurcose@gmail.com>
2025-08-27 11:43:47 -0400

    Adjust shop prices and encounter rates

--- mods/fishing/encounter/entry.lua
+++ mods/fishing/encounter/entry.lua
@@ -22,9 +22,9 @@ local pool_weights = {
   -- bait level 2
   { 1, 2, 0, 0, 0 },
   -- bait level 3
-  { 0, 1, 3, 0, 0 },
+  { 0, 1, 4, 0, 0 },
   -- bait level 4
-  { 0, 1, 1, 4, 0 },
+  { 0, 1, 2, 6, 0 },
   -- bait level 5
   { 0, 0, 1, 6, 1 },
 }
