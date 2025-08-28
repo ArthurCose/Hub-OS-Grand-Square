5c505743ebfbd1a7c764b2afc592c3df22a22280
Arthur Cosentino <arthurcose@gmail.com>
2025-08-28 07:46:01 -0400

    More behavior rebalancing

--- mods/fishing/encounter/fish_data.lua
+++ mods/fishing/encounter/fish_data.lua
@@ -29,20 +29,20 @@ fish_data.Piranha = {
   [Rank.SP] = {
     name = "Piranha",
     rank = "SP",
-    slide_timing = { { 4, 10 }, { 4, 6 } },
-    fight_timing = { { 6, 8 }, { 4, 6 } },
+    slide_timing = { { 8, 10 }, { 4, 6 } },
+    fight_timing = { { 6, 8 } },
   },
   [Rank.Rare1] = {
     name = "Piranha",
     rank = "Rare1",
-    slide_timing = { { 3, 8 } },
-    fight_timing = { { 3, 5 }, { 2, 4 } },
+    slide_timing = { { 8, 10 }, { 4, 6 } },
+    fight_timing = { { 5, 6 }, { 4, 4 } },
   },
   [Rank.Rare2] = {
     name = "Piranha",
     rank = "Rare2",
-    slide_timing = { { 3, 8 } },
-    fight_timing = { { 2, 4 }, { 2, 3 } },
+    slide_timing = { { 6, 8 }, { 4, 8 } },
+    fight_timing = { { 5, 6 }, { 2, 4 } },
   }
 }
 
