aceafd843ac6d7b6daf3b8c814ba2a0e838f85f2
Arthur Cosentino <arthurcose@gmail.com>
2025-08-28 22:29:47 -0400

    Revert hooked fish reservation logic

--- mods/fishing/hooked_fish_status/entry.lua
+++ mods/fishing/hooked_fish_status/entry.lua
@@ -17,17 +17,7 @@ function status_init(status)
 
   status.on_delete_func = function()
     original_tile:add_entity(fish)
-
-    -- switch component to handle removing reservation
-    component.on_update_func = function()
-      if fish:has_actions() then
-        -- piranhas will try to shoot while hooked,
-        -- causing it to wait until the action ends before reserving the original tile
-        return
-      end
-
-      original_tile:remove_reservation_for(fish)
-      component:eject()
-    end
+    original_tile:remove_reservation_for(fish)
+    component:eject()
   end
 end
